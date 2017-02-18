23e1f15257773e24054a8b998c522280e5e9e839
faqihsalban <faqih.salban@gmail.com>
2017-02-18 22:45:38 +0700

    a

--- conection.php
+++ conection.php
@@ -14,8 +14,27 @@
 class conection {
    
    public static function getconection(){
+
+            $connectstr_dbhost = '';
+            $connectstr_dbname = '';
+            $connectstr_dbusername = '';
+            $connectstr_dbpassword = '';
+
+            foreach ($_SERVER as $key => $value) {
+                if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+                    continue;
+                }
+                
+                $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+                $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+                $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+                $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+            }
+
+            $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
         try{
-          $db_handler= new PDO("mysql:host=127.0.0.1:49233; Database = delibox", "azure", "password");
+          $db_handler= new PDO('mysql:host='$connectstr_dbhost; $connectstr_dbname, $connectstr_dbusername,  $connectstr_dbpassword);
           $db_handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           return $db_handler;
         }
